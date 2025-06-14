b4173f88a6c2bcbbf6617e36556c0f4f480b5baf
Heldzs <helderj118@gmail.com>
2025-06-13 20:49:39 -0400

    angular 13/06

---
+++ frontend/src/app/components/tasklistComponent/tasklist.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tasklist',
+  standalone: true,
+  imports: [],
+  templateUrl: './tasklist.html',
+  styleUrl: './tasklist.css'
+})
+export class TasklistComponent {
+
+}
